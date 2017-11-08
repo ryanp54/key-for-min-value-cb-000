1759424383ea23b11aac08809f6bfda0a0aaf74b
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 06:16:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   name_hash.collect do |key, value|
     if min_key
-      min_value > value ? min_key, min_value = key, value
+      min_key, min_value = key, value if min_value > value
     else
       min_key, min_value = key, value
   end
